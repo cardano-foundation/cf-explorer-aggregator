a57539e8e166018b7b6fc0e3e8db30032b1e949c
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-10-11 08:20:26 +0200

    using commit event to flush to db and clean up

--- src/main/java/org/cardanofoundation/cfexploreraggregator/txcount/model/repository/AddressTxCountRepository.java
+++ src/main/java/org/cardanofoundation/cfexploreraggregator/txcount/model/repository/AddressTxCountRepository.java
@@ -3,8 +3,6 @@ package org.cardanofoundation.cfexploreraggregator.txcount.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import org.cardanofoundation.cfexploreraggregator.txcount.model.entity.AddressTxCountEntity;
 
@@ -13,10 +11,4 @@ public interface AddressTxCountRepository extends JpaRepository<AddressTxCountEn
     List<AddressTxCountEntity> findByAddressOrderBySlotDesc(String address);
 
     void deleteBySlotGreaterThan(long slot);
-
-    @Query(nativeQuery = true, value = """
-        DELETE FROM address_tx_count WHERE slot < :slot
-        AND address IN (SELECT address FROM address_tx_count GROUP BY address HAVING COUNT(*) > 1)
-        """)
-    void cleanUpDB(@Param("slot") long slot);
 }
--- src/main/java/org/cardanofoundation/cfexploreraggregator/txcount/processor/AddressTxCountProcessor.java
+++ src/main/java/org/cardanofoundation/cfexploreraggregator/txcount/processor/AddressTxCountProcessor.java
@@ -5,11 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import jakarta.annotation.PostConstruct;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +23,7 @@ import com.bloxbean.cardano.yaci.helper.model.Utxo;
 import com.bloxbean.cardano.yaci.store.events.ByronMainBlockEvent;
 import com.bloxbean.cardano.yaci.store.events.RollbackEvent;
 import com.bloxbean.cardano.yaci.store.events.TransactionEvent;
+import com.bloxbean.cardano.yaci.store.events.internal.CommitEvent;
 
 import org.cardanofoundation.cfexploreraggregator.txcount.model.entity.AddressTxCountEntity;
 import org.cardanofoundation.cfexploreraggregator.txcount.model.repository.AddressTxCountRepository;
@@ -45,16 +42,16 @@ public class AddressTxCountProcessor {
 
     private final ConcurrentHashMap<String, Tuple<Long, Long>> hashCounts = new ConcurrentHashMap<>();
 
-    private final ScheduledExecutorService dbWriteScheduler = Executors.newScheduledThreadPool(1);
+//    private final ScheduledExecutorService dbWriteScheduler = Executors.newScheduledThreadPool(1);
 
     @Value("${explorer.aggregation.addressTxCount.Safe-Slot-Distance}")
     private long safeSlotDistance;
-
-    @PostConstruct
-    private void init() {
-        // flushing every 10 seconds to the DB
-        dbWriteScheduler.scheduleAtFixedRate(this::flushToDatabase, 10, 10, TimeUnit.SECONDS);
-    }
+//
+//    @PostConstruct
+//    private void init() {
+//        // flushing every 10 seconds to the DB
+//        dbWriteScheduler.scheduleAtFixedRate(this::flushToDatabase, 10, 10, TimeUnit.SECONDS);
+//    }
 
     @EventListener
     @Transactional
@@ -85,11 +82,13 @@ public class AddressTxCountProcessor {
         });
     }
 
-    private void flushToDatabase() {
+    @EventListener
+    @Transactional
+    public void handleCommitEvent(CommitEvent commitEvent) {
         try {
             Map<String, Tuple<Long, Long>> snapshot = new HashMap<>(hashCounts);
             hashCounts.clear();
-
+            // flush to db
             List<AddressTxCountEntity> entities = snapshot.entrySet().stream()
                     .map(entry -> {
                         List<AddressTxCountEntity> byAddressOrderByTxCountDesc = addressTxCountRepository.findByAddressOrderBySlotDesc(entry.getKey());
@@ -100,6 +99,10 @@ public class AddressTxCountProcessor {
                             slot = byAddressOrderByTxCountDesc.getFirst().getSlot();
                         }
 
+                        // Delete old records in save distance
+                        List<AddressTxCountEntity> toBeDeleted = byAddressOrderByTxCountDesc.stream().filter(addressTxCountEntity -> addressTxCountEntity.getSlot() < commitEvent.getMetadata().getSlot() - safeSlotDistance).toList();
+                        addressTxCountRepository.deleteAll(toBeDeleted);
+
                         return AddressTxCountEntity.builder()
                                 .address(entry.getKey())
                                 .txCount(txCount + entry.getValue()._2)
@@ -109,12 +112,17 @@ public class AddressTxCountProcessor {
                     .toList();
             addressTxCountRepository.saveAll(entities);
             log.info("Saved {} address tx count entities", entities.size());
-            // getting the max slot from the snapshot and cleaning up the DB
-            snapshot.entrySet().stream().max((o1, o2) -> (int) (o1.getValue()._1 - o2.getValue()._1))
-                    .ifPresent(entry -> addressTxCountRepository.cleanUpDB(entry.getValue()._1 - safeSlotDistance));
+
+            // cleaning DB
+
         } catch (Exception e) {
             log.error("Error flushing to database");
         }
+
+    }
+
+    private void flushToDatabase() {
+
     }
 
     @EventListener
